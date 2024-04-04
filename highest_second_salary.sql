316eb162ebef8f0282c4bbc406340ba0b0b489cd
AnushaMeka95 <anushameka8595@gmail.com>
2024-04-03 23:36:05 -0500

    add-sql-question

---
+++ highest_second_salary.sql
@@ -0,0 +1,9 @@
+-- Given a table employess, with employee name and salary columns, 
+-- Query the second higest salary amoong the employesss
+
+--employee_name, salary
+--Chandra, 100
+--Anusha, 200
+--Krishna, 150
+--
+--Ans: Krishna, 150
